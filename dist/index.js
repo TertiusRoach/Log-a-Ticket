4d34870970c0cceabbedcb5150e4c5566ef5962d
Tertius Roach <tertius.roach@gmail.com>
2021-07-08 20:21:58 +0200

    Adding Font Awesome essentials

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+define(["require", "exports", "tools/get-elements"], function (require, exports, get_elements_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var pIfrele = get_elements_1.iframes.iGet('page').iframeContainer;
+});
+
+//# sourceMappingURL=dist/index.js.map
